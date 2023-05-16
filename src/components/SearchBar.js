f5432e0b22903c591f78a6376dc1eccb7fd8f2de
Elvira Xiong <47922282+rxiong19@users.noreply.github.com>
2023-05-16 13:55:25 -0400

    fixed the checking condition for navigate in search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,33 +9,35 @@ export default function SearchBar() {
   const [SearchTerm, setSearchTerm] = useState("");
   const [SearchResult, setSearchResult] = useState("");
   const navigate = useNavigate();
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")).id);
+  const [cur_user, setCur_User] = useState(
+    JSON.parse(localStorage.getItem("user")).id
+  );
   //need to figure how to get users
   const items = [
     {
       id: "911952f9-6075-495e-99ba-255f25fd0e52",
       name: "Jack Chen",
-      img: user.Photo ? user.Photo : picLink,
+      img: cur_user.Photo ? cur_user.Photo : picLink,
     },
     {
       id: "c08f6630-7ff2-4589-b683-b2481e24ab58",
       name: "John Doe",
-      img: user.Photo ? user.Photo : picLink,
+      img: cur_user.Photo ? cur_user.Photo : picLink,
     },
     {
       id: "ec2a01ee-754f-45a7-ab71-af116af291b1",
       name: "Brian Zhang",
-      img: user.Photo ? user.Photo : picLink,
+      img: cur_user.Photo ? cur_user.Photo : picLink,
     },
     {
       id: 3,
       name: "PHP",
-      img: user.Photo ? user.Photo : picLink,
+      img: cur_user.Photo ? cur_user.Photo : picLink,
     },
     {
       id: 4,
       name: "Elvira",
-      img: user.Photo ? user.Photo : picLink,
+      img: cur_user.Photo ? cur_user.Photo : picLink,
     },
   ];
 
@@ -56,8 +58,10 @@ export default function SearchBar() {
   const handleOnSelect = (item) => {
     // the item selected
     setSearchTerm(item);
-    let cur_user = JSON.parse(localStorage.getItem("user")).id;
-    navigate(`/profile/${cur_user !== "" ? "?" : item.id}`);
+    console.log(cur_user);
+    navigate(
+      `/profile/${cur_user === item.id && cur_user !== "" ? "?" : item.id}`
+    );
     //console.log(item);
   };
 
@@ -79,9 +83,11 @@ export default function SearchBar() {
     <div>
       <form
         onSubmit={() => {
-          let cur_user = JSON.parse(localStorage.getItem("user")).id;
-
-          navigate(`/profile/${cur_user !== "" ? "?" : SearchResult}`);
+          navigate(
+            `/profile/${
+              SearchResult === cur_user && cur_user !== "" ? "?" : SearchResult
+            }`
+          );
         }}
         style={{ display: "flex", alignItems: "center" }}
       >
